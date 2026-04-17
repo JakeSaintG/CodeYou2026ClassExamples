64182543cad3811c963c92cfe8b24dc65dab44af
jakesaintg <jake@stgermain.dev>
2026-04-16 20:41:15 -0400

    EOD commit

---
+++ Day6/app.js
@@ -0,0 +1,38 @@
+const container = document.getElementById("container1");
+
+const flip = () => {
+    if (container.style.flexDirection !== "column-reverse") {
+        container.style.flexDirection = "column-reverse";
+    } else {
+        container.style.flexDirection = "column";
+    }
+};
+
+thisIsAnExampleVariable = 'foo'
+
+let cat = "column";
+cat = "column";
+cat = "column";
+cat = "column";
+cat = "column";
+cat = "column";
+cat = "column";
+cat = "column";
+cat = "column";
+cat = "column";
+cat = "column";
+cat = "column";
+cat = "column";
+cat = "column";
+
+const json = {
+    foo: 1,
+    bar: 1,
+    baz: 1,
+    quxx: 1,
+};
+
+const foo = json.foo;
+const bar = json.bar;
+const baz = json.baz;
+const quxx = json.quxx;
