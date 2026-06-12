1d2955db2f140406ce028a71e1fe0f282d594296
jakesaintg <jake@stgermain.dev>
2026-06-12 19:58:19 -0400

    end of class commit

---
+++ Day12/README.md
@@ -0,0 +1,6 @@
+create a env.js file in Day12
+add api key from project submission
+start page
+- via NODE
+- via live server
+
---
+++ Day12/app.js
@@ -0,0 +1,5 @@
+import API_KEY from './env.js';
+
+console.log(API_KEY);
+
+document.getElementById('print_api_key').innerText = apiKey;
